a45f6d965af1b67a5585afc3b9de782f1e36a2c1
jinwoo park <swat018@gmail.com>
2022-01-15 13:20:54 +0900

    feat : 스프링 부트 애플리케이션 도커 이미지 만들기

---
+++ src/main/java/com/example/demospringboot23/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demospringboot23;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Period;
+
+@RestController
+public class HelloController {
+
+    @Value("${timeToFry}")
+    private Period timeToFry;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "chicken " + timeToFry;
+    }
+}
