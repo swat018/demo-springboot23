af37d15100ee585a45385734d0db83aa507165e2
jinwoo park <swat018@gmail.com>
2022-01-18 23:45:55 +0900

    feat : Liveness와 Readiness

--- src/main/java/com/example/demospringboot23/DemoSpringboot23Application.java
+++ src/main/java/com/example/demospringboot23/DemoSpringboot23Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Period;
 
+@EnableAsync
 @SpringBootApplication
 public class DemoSpringboot23Application {
 
