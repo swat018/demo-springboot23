b57aae94ef962e9b7f6f1765c147ab39acd887ef
jinwoo park <swat018@gmail.com>
2022-01-24 07:51:44 +0900

    fix : 스프링 부트와 ㅏ쿠버네티븟 ConfigMap 연동

--- src/main/java/com/example/demospringboot23/ServiceProperties.java
+++ src/main/java/com/example/demospringboot23/ServiceProperties.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConstructorBinding
-@ConfigurationProperties(prefix = "service")
+@ConfigurationProperties(prefix = "my")
 public class ServiceProperties {
 
     private String message;
